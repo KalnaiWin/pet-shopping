b33b4716f1d5e8d5046fa66bf3b489e6d4b1e492
Phúc <nhbphuc2006@gmail.com>
2025-09-09 14:54:17 +0700

    Updated 10

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -18,11 +18,11 @@ export const auth = betterAuth({
     secret: process.env.BETTER_AUTH_SECRET,
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET_KEY as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET_KEY as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
   },
   emailAndPassword: {
