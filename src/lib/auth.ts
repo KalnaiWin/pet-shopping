11d17942ba6792deea8f92c59f5a207f82eceb0f
Phúc <nhbphuc2006@gmail.com>
2025-09-09 15:08:47 +0700

    Updated 11

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -15,14 +15,14 @@ export const auth = betterAuth({
     provider: "postgresql",
   }),
   socialProviders: {
-    secret: process.env.BETTER_AUTH_SECRET,
+    secret: process.env.BETTER_AUTH_SECRET!,
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID! as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET! as string,
     },
     github: {
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      clientId: process.env.GITHUB_CLIENT_ID! as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET! as string,
     },
   },
   emailAndPassword: {
